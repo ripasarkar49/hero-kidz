da5596ad2e2f0a624927b9a19cb6fad919e41895
unknown <ripa sarkar>
2026-02-24 16:00:33 +0600

    Button of navbar

--- src/components/Buttons/Navlink.jsx
+++ src/components/Buttons/Navlink.jsx
@@ -1,10 +1,12 @@
+"use client";
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React, { Children } from 'react'
 
-export default function Navlink() {
-      const path=usePathname();
+export default function Navlink({ href, children }) {
+      const path=usePathname()
   return (
   
-    <Link href={href} className={`${path.startsWith(href) && 'text-primary'} 'text-base-content'}`}>{Children}</Link>
+    <Link href={href} className={`${path.startsWith(href) ? 'text-primary' : 'text-base-content'}`}>{children}</Link>
   )
 }
--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 import Logo from "./Logo";
-
+import Navlink from "../Buttons/Navlink";
+import Link from "next/link";
+import { FaCartShopping } from "react-icons/fa6";
 export default function Navbar() {
+  const nav = (
+    <>
+      <li>
+        <Navlink href="/">Home</Navlink>
+      </li>
+      <li>
+        <Navlink href="products">Products</Navlink>
+      </li>
+      <li>
+        <Navlink href="blog">Blog</Navlink>
+      </li>
+      <li>
+        <Navlink href="contact">Contact</Navlink>
+      </li>
+    </>
+  );
   return (
     <div>
       <div className="navbar bg-base-100 ">
@@ -28,52 +46,21 @@ export default function Navbar() {
               tabIndex="-1"
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
-                <a>Item 1</a>
-              </li>
-              <li>
-                <a>Parent</a>
-                <ul className="p-2">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a>Item 3</a>
-              </li>
+              {nav}
             </ul>
           </div>
-         <Logo></Logo>
+          <Logo></Logo>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <details>
-                <summary>Parent</summary>
-                <ul className="p-2 bg-base-100 w-40 z-1">
-                  <li>
-                    <a>Submenu 1</a>
-                  </li>
-                  <li>
-                    <a>Submenu 2</a>
-                  </li>
-                </ul>
-              </details>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
+          <ul className="menu menu-horizontal px-1">{nav}</ul>
         </div>
-        <div className="navbar-end">
-          <a className="btn">Button</a>
+        <div className="navbar-end space-x-2">
+          <Link href="/carts" className="btn btn-primary">
+            <FaCartShopping />
+          </Link>
+          <Link href="/login" className="btn btn-primary btn-outline">
+            Login
+          </Link>
         </div>
       </div>
     </div>
