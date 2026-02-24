2f1619bb52b03f1c0dab8a1eb736d18c998a94fa
Ripa Sarkar <ripasarkar49@gmail.com>
2026-02-24 23:55:46 +0600

    upgrade

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -4,6 +4,8 @@ export default function Products() {
   return (
     <div>
         <h2 className="text-3xl font-bold text-center mb-10">Our Products</h2>
+        <h2 className="text-3xl font-bold text-center mb-10">Our Products</h2>
+        <h2 className="text-3xl font-bold text-center mb-10">Our Products</h2>
     </div>
   )
 }
